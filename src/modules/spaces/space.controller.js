5d49211a32c3cf5b8470c8fc5ffd5249efa0c0b4
William Daniel Dias <daniel.willia@hotmail.com>
2025-11-25 22:00:00 -0300

    feito

--- src/modules/spaces/space.controller.js
+++ src/modules/spaces/space.controller.js
@@ -1,3 +1,106 @@
+import { makeSpaceService } from "./space.service.js"
+
 export const makeSpaceController = () => {
-    
+    const spaceService = makeSpaceService()
+
+    const create = async (req, res, next) => {
+        try {
+            const supporterId = req.user?.id
+
+            const space = await spaceService.createSpace({
+                supporterId,
+                name: req.body.name,
+                address: req.body.address,
+                capacity: req.body.capacity,
+                photoUrl: req.body.photoUrl,
+                description: req.body.description
+            })
+
+            return res.status(201).json({
+                message: 'Space created successfully.',
+                space
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    const getById = async (req, res, next) => {
+        try {
+            const { id } = req.params
+            const space = await spaceService.getSpaceById(id)
+
+            if (!space) {
+                return res.status(404).json({
+                    message: 'Space not found.',
+                    errorCode: 'SPACE_NOT_FOUND'
+                })
+            }
+
+            return res.status(200).json({ space })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    const getBySupporter = async (req, res, next) => {
+        try {
+            const { supporterId } = req.params
+            const spaces = await spaceService.getSpacesBySupporter(Number(supporterId))
+            return res.status(200).json({ spaces })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    const getAll = async (req, res, next) => {
+        try {
+            const spaces = await spaceService.getAllSpaces()
+            return res.status(200).json({ spaces })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    const update = async (req, res, next) => {
+        try {
+            const { id } = req.params
+            const requesterId = req.user.id // from auth middleware
+
+            const updated = await spaceService.updateSpace({
+                id,
+                requesterId,
+                data: req.body
+            })
+
+            return res.status(200).json({
+                message: 'Space updated successfully.',
+                space: updated
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    const remove = async (req, res, next) => {
+        try {
+            const { id } = req.params
+            const requesterId = req.user.id
+
+            await spaceService.deleteSpace({ id, requesterId })
+
+            return res.status(204).send()
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    return {
+        create,
+        getById,
+        getBySupporter,
+        getAll,
+        update,
+        remove
+    }
 }
--- src/routes/space.routes.js
+++ src/routes/space.routes.js
@@ -2,15 +2,14 @@
 import { Router } from 'express'
 import { makeSpaceService } from '../modules/spaces/space.service.js'
 import { makeSpaceController } from '../modules/spaces/space.controller.js'
-import { spaceSchemas } from '../modules/spaces/space.schemas.js'
 import { ensureAuth } from '../middlewares/auth.js'
 import { validate } from '../middlewares/validate.js'
+import { createSpaceSchema, updateSpaceSchema } from '../modules/spaces/space.schemas.js'
 
 export const spaceRouter = () => {
     const router = Router()
 
-    const spaceService = makeSpaceService()
-    const spaceController = makeSpaceController({ spaceService })
+    const spaceController = makeSpaceController()
 
     // Public
     router.get('/', spaceController.getAll)
@@ -21,14 +20,14 @@ export const spaceRouter = () => {
     router.post(
         '/',
         ensureAuth,
-        validate({ body: spaceSchemas.createSpace }),
+        validate({ body: createSpaceSchema }),
         spaceController.create
     )
 
     router.put(
         '/:id',
         ensureAuth,
-        validate({ body: spaceSchemas.updateSpace }),
+        validate({ body: updateSpaceSchema }),
         spaceController.update
     )
 
